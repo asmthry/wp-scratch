684173a48402067b617230cfdb6a4d6d7d766f0d
asmthry <asmthry@gmail.com>
2023-11-07 21:57:19 +0530

    ADD PLUGIN BASIC FILES

---
+++ wp-scratch.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:          WP Scratch (asmthry)
+ * Plugin URI:           https://github.com/asmthry/wp-scratch.git
+ * Description:          This plugin will help you reduce your coding effort.
+ * Version:              0.0.1
+ * Requires at least:    6.3
+ * Requires PHP:         8.0
+ * Author:               ASMTHRY
+ * License:              MIT
+ * License URI:          https://opensource.org/license/mit/
+ *
+ * @package WP Scratch
+ */
+
+defined( 'ABSPATH' ) || die( 'What are you doing here? No direct access allowed.' );
+
+/** Define Constructors For ASMTHRY WP Scratch plugin */
+define( 'ASMTHRY_PLUGIN_VERSION', '0.0.1' );
